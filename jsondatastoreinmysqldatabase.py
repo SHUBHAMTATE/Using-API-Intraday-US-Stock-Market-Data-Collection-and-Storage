4ae1778217b7fbebdec952fd721f40fea8601178
Shubham Tate <92684193+SHUBHAMTATE@users.noreply.github.com>
2024-07-27 19:53:43 +0530

    Update jsondatastoreinmysqldatabase.py

--- jsondatastoreinmysqldatabase.py
+++ jsondatastoreinmysqldatabase.py
@@ -1,4 +1,5 @@
 import requests
+#
 def get(company_name,intervel):
     #requesting for api on www.alphavantage.com
     response=requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol="+company_name+"&interval="+intervel+"min&apikey=S5EH3VNQ0WZ5KR74")
